065d74cc79e8e4ed51ccb5798d334b7fa591b443
razqq <croitorurazvan1@gmail.com>
2021-09-29 13:01:11 +0300

    try another

--- index.js
+++ index.js
@@ -1,28 +1,94 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const {WebhookClient} = require('dialogflow-fulfillment');
+const express = require('express');
+const app = express();
+const dfff = require('dialogflow-fulfillment');
 
-const app = express()
-app.use(bodyParser.json())
-const port = process.env.PORT || 3000
+var admin = require("firebase-admin");
 
-app.post('/dialogflow-fulfillment', (request, response) => {
-    dialogflowFulfillment(request, response)
-})
+var serviceAccount = require('./config/stbot-31fd4-firebase-adminsdk-a79b5-1bd144a965.json');
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://stbot-31fd4.firebaseio.com"
+  });
 
-const dialogflowFulfillment = (request, response) => {
-    const agent = new WebhookClient({request, response})
+  console.log("Connected to DB");
+  
+} catch (error) {
+  console.log("Error here" + error);
+  
+}
 
-    function sayHello(agent){
-        agent.add("Hello, this was a nice tutorial by axlewebtech")
+var db = admin.firestore();
+
+app.get('/', (req, res)=>{
+    res.send("We are live")
+});
+
+app.post('/', express.json(), (req, res)=>{
+    const agent = new dfff.WebhookClient({
+        request : req,
+        response : res
+    });
+
+    function demo(agent){
+        agent.add("Sending response from Webhook server as v1.1.11.1");
+    }
+    function customPayloadDemo(agent){
+        var payloadData = {
+            "richContent": [
+              [
+                {
+                  "type": "accordion",
+                  "title": "Accordion title",
+                  "subtitle": "Accordion subtitle",
+                  "image": {
+                    "src": {
+                      "rawUrl": "https://example.com/images/logo.png"
+                    }
+                  },
+                  "text": "Accordion text"
+                }
+              ]
+            ]
+          }
+
+          agent.add( new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))
     }
 
-    let intentMap = new Map();
-    intentMap.set("Default Welcome Intent", sayHello)
-    agent.handleRequest(intentMap)
+    function finalConfirmation(agent){
+      var name = agent.context.get("awaiting_name").parameters['given-name'];
+      var email = agent.context.get("awaiting_email").parameters.email;
 
-}
+      console.log(name);
+      console.log(email);
+
+
+      
+
+      agent.add(`Hello ${name}, your email: ${email}. We confirmed your meeting.`);
+
+      return db.collection('meeting').add({
+        name : name,
+        email : email,
+        time : Date.now()
+      }).then(ref =>
+
+        //fetching free slots from G-cal
+        console.log("Meeting details added to DB")
+        )
+
+    }
+
+
+    var intentMap = new Map();
+    intentMap.set('finalConfirmation', finalConfirmation)
+    intentMap.set('webhookDemo',demo )
+    intentMap.set('customPayloadDemo', customPayloadDemo)
+    
+
+    agent.handleRequest(intentMap);
+
+});
+
+app.listen(3333, ()=>console.log("Server is live at port 3333"));
