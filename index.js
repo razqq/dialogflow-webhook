fde7f2bec3ce32ac1062ce8ecbe8662cfee0e514
razqq <croitorurazvan1@gmail.com>
2021-09-29 13:14:15 +0300

    fix

--- index.js
+++ index.js
@@ -2,24 +2,6 @@ const express = require('express');
 const app = express();
 const dfff = require('dialogflow-fulfillment');
 
-var admin = require("firebase-admin");
-
-var serviceAccount = require('./config/stbot-31fd4-firebase-adminsdk-a79b5-1bd144a965.json');
-
-try {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://stbot-31fd4.firebaseio.com"
-  });
-
-  console.log("Connected to DB");
-  
-} catch (error) {
-  console.log("Error here" + error);
-  
-}
-
-var db = admin.firestore();
 
 app.get('/', (req, res)=>{
     res.send("We are live")
@@ -56,33 +38,7 @@ app.post('/', express.json(), (req, res)=>{
           agent.add( new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))
     }
 
-    function finalConfirmation(agent){
-      var name = agent.context.get("awaiting_name").parameters['given-name'];
-      var email = agent.context.get("awaiting_email").parameters.email;
-
-      console.log(name);
-      console.log(email);
-
-
-      
-
-      agent.add(`Hello ${name}, your email: ${email}. We confirmed your meeting.`);
-
-      return db.collection('meeting').add({
-        name : name,
-        email : email,
-        time : Date.now()
-      }).then(ref =>
-
-        //fetching free slots from G-cal
-        console.log("Meeting details added to DB")
-        )
-
-    }
-
-
     var intentMap = new Map();
-    intentMap.set('finalConfirmation', finalConfirmation)
     intentMap.set('webhookDemo',demo )
     intentMap.set('customPayloadDemo', customPayloadDemo)
     
